9e74c65fc412d9c492fd788a1ee8608750ab29c4
Kian.Abdalkhani <kabdalkhani@gmail.com>
2025-07-21 17:11:04 -0700

    Refactor bot initialization by replacing `create_bot` with `MyClient` class for improved structure and extensions support

--- src/main.py
+++ src/main.py
@@ -1,31 +1,75 @@
+import os
 import logging
 import sys
-import os
-import asyncio
 
 from dotenv import load_dotenv
 
-from src.bot import create_bot
+import discord
+from discord.ext import commands
+
 from src.utils.logging import setup_logging
+from src.config.settings import GUILD_ID
+from src.services.bot_llm import bot_response
+
+load_dotenv()
+setup_logging()
+logger = logging.getLogger(__name__)
+
+class MyClient(commands.Bot):
+
+    def __init__(self) -> None:
+        # set the bot intents
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        super().__init__(command_prefix='!', intents=intents)
+
+        self.guild = discord.Object(id=GUILD_ID)
+
+    async def on_ready(self):
+        logger.info(f"{self.user} ready for commands")
+
+        extensions = [
+            'src.cogs.story_teller'
+        ]
+
+        for extension in extensions:
+            try:
+                await self.load_extension(extension)
+                logger.info(f"Loaded extension: {extension}")
+            except Exception as e:
+                logger.error(f"Failed to load extension {extension}: {e}")
+
+        try:
+            synced = await self.tree.sync(guild=self.guild)
+            logger.info(f"Synced {len(synced)} commands")
+        except Exception as e:
+            logger.error(f"Error syncing commands: {e}")
+
+    async def on_connect(self):
+        logger.info(f"{self.user} connected to discord successfully")
+
+    async def on_disconnect(self):
+        logger.info(f"{self.user} disconnected from discord")
+
+    async def on_message(self,message):
+        if message.author == self.user:
+            return
+        if self.user.mentioned_in(message):
+            logger.info(f"{message.author} mentioned bot in {message.channel}")
+            response = bot_response(prompt=message.content)
+            await message.channel.send(response)
 
 
-async def async_main():
-
-    load_dotenv()
-    setup_logging()
-    bot = create_bot()
-
-    return bot
 
 def main():
-    loop = asyncio.new_event_loop()
-    bot = loop.run_until_complete(async_main())
-
-    bot_token = os.getenv("BOT_TOKEN")
+    # pass bot token to Client
+    bot_token = os.getenv("TEST_TOKEN")
     if not bot_token:
         logging.error("No bot token found in environment variables")
         sys.exit(1)
-    bot.run(bot_token)
+    client = MyClient()
+    client.run(bot_token)
 
 if __name__ == "__main__":
     main()
